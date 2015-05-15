96f173a1dc9398643db584db7e49bb324fffe33a
daiki <dev.da.iz.0x@gmail.com>
2015-05-16 01:15:52 +0900

    Nightly

---
+++ make_ui_table.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Project Apricot
+# Copyright (c) 2015 Daiki, Takanori.
+
+import sys
+import os.path
+from PIL import Image
+
+# 画像のパスが有効であることを確認して返す
+def get_img_path():
+  if len(sys.argv) == 2:
+      img_path = sys.argv[1]
+      if os.path.exists(img_path):
+          return img_path
+  else:
+      return None
+
+# 画像の基本情報を取得して返す
+def get_img_info(path):
+    img = Image.open(path)
+    imgInfo = {
+      "size_h": img.size[1],
+      "size_w": img.size[0],
+      "mode": img.mode,
+      "format": img.format
+    }
+    return imgInfo
+
+if __name__ == '__main__':
+    src = get_img_path()
+    if src != None:
+        print(get_img_info(src))
