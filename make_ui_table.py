09b74805660ae374d7a4abcd6928053cf1d4376e
daiki <dev.da.iz.0x@gmail.com>
2015-05-16 01:18:14 +0900

    ミスってた

--- make_ui_table.py
+++ make_ui_table.py
@@ -10,12 +10,11 @@ from PIL import Image
 
 # 画像のパスが有効であることを確認して返す
 def get_img_path():
-  if len(sys.argv) == 2:
-      img_path = sys.argv[1]
-      if os.path.exists(img_path):
-          return img_path
-  else:
-      return None
+    if len(sys.argv) == 2:
+        img_path = sys.argv[1]
+        if os.path.exists(img_path):
+            return img_path
+    return None
 
 # 画像の基本情報を取得して返す
 def get_img_info(path):
